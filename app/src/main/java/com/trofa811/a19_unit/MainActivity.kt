82cda73ab16e7830414083f64beb2d1504819742
Wasp <trofa811@gmail.com>
2022-11-06 15:08:30 +0300

    Initial commit

---
+++ app/src/main/java/com/trofa811/a19_unit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trofa811.a19_unit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
